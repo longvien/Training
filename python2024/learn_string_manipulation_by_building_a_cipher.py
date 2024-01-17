9d5dc491412246a3c49401544abf942af0f6e8b8
Long Vien <vienngoclongvn@gmail.com>
2024-01-17 18:40:36 +0100

    first commit

---
+++ python2024/learn_string_manipulation_by_building_a_cipher.py
@@ -0,0 +1,9 @@
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+for char in text.lower():
+    index = alphabet.find(char)
+    print(char, index)
+    new_index = index + shift
+    new_char = alphabet[new_index]
