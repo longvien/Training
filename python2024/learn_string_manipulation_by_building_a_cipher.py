e8b4abc357e7386fe71c42a5dd896c61eb8f4191
Long Vien <vienngoclongvn@gmail.com>
2024-01-22 21:08:07 +0100

    completed exercise

--- python2024/learn_string_manipulation_by_building_a_cipher.py
+++ python2024/learn_string_manipulation_by_building_a_cipher.py
@@ -1,4 +1,4 @@
-text = 'Hello Zaira!'
+text = 'mrttaqrhknsw ih puggrur'
 custom_key = 'python'
 
 def vigenere(message, key, direction=1):
@@ -23,10 +23,13 @@ def vigenere(message, key, direction=1):
             final_message += alphabet[new_index]
     
     return final_message
-def encrypt(message, key):
-    pass
 
-encryption = vigenere(text, custom_key)
-print(encryption)
-decryption = vigenere(encryption, custom_key, -1)
-print(decryption)
+def encrypt(message, key):
+    return vigenere(message, key)
+    
+def decrypt(message, key):
+    return vigenere(message, key, -1)
+print(f'Encrypted text: {text}')
+print(f'Key: {custom_key}')
+decryption = decrypt(text, custom_key)
+print(f'Decrypted text: {decryption}')
