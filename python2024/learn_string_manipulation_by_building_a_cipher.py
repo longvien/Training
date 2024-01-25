e232c8d343ef7738099988a0afc4e52b29866607
Long Vien <vienngoclongvn@gmail.com>
2024-01-25 20:17:32 +0100

    completed exercise

--- python2024/learn_string_manipulation_by_building_a_cipher.py
+++ python2024/learn_string_manipulation_by_building_a_cipher.py
@@ -1,8 +1,8 @@
 text = 'mrttaqrhknsw ih puggrur'
-custom_key = 'python'
+custom_key = 'happycoding'
 
 def vigenere(message, key, direction=1):
-    key_index = 1
+    key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     final_message = ''
 
@@ -29,7 +29,8 @@ def encrypt(message, key):
     
 def decrypt(message, key):
     return vigenere(message, key, -1)
-print(f'Encrypted text: {text}')
+
+print(f'\nEncrypted text: {text}')
 print(f'Key: {custom_key}')
 decryption = decrypt(text, custom_key)
-print(f'Decrypted text: {decryption}')
+print(f'\nDecrypted text: {decryption}\n')
