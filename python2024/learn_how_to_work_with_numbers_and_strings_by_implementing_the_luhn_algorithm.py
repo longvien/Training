ba964aacad4930417eb982df7a50d911c64fc370
Long Vien <vienngoclongvn@gmail.com>
2024-01-26 18:54:04 +0100

    completed exercise

---
+++ python2024/learn_how_to_work_with_numbers_and_strings_by_implementing_the_luhn_algorithm.py
@@ -0,0 +1,15 @@
+def verify_card_number(card_number):
+    sum_of_odd_digits = 0
+    card_number_reversed = card_number[::-1]
+    odd_digits = card_number_reversed[::2]
+
+    print(odd_digits)
+    
+def main():
+    card_number = '4111-1111-4555-1142'
+    card_translation = str.maketrans({'-': '', ' ': ''})
+    translated_card_number = card_number.translate(card_translation)
+
+    verify_card_number(translated_card_number)
+
+main()
