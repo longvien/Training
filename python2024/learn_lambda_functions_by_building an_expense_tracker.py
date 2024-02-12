010270ed92be242e354088c2d3714adb0a7e2d65
Long Vien <vienngoclongvn@gmail.com>
2024-02-12 22:16:48 +0100

    completed exercise

--- python2024/learn_lambda_functions_by_building an_expense_tracker.py
+++ python2024/learn_lambda_functions_by_building an_expense_tracker.py
@@ -16,7 +16,7 @@ def main():
     expenses = []
     while True:
         print('\nExpense Tracker')
-        print('1. Add an expense')
+        print('1. Add an expens')
         print('2. List all expenses')
         print('3. Show total expenses')
         print('4. Filter expenses by category')
