2e664cc1a6b30c93e000f5f2e4a8fe64dab50760
Long Vien <vienngoclongvn@gmail.com>
2024-02-15 21:47:59 +0100

    completed exercise

--- python2024/Learn_Python_List_Comprehension_by_Building_a_Case_Converter_Program.py
+++ python2024/Learn_Python_List_Comprehension_by_Building_a_Case_Converter_Program.py
@@ -1,5 +1,5 @@
 def convert_to_snake_case(pascal_or_camel_cased_string):
     snake_cased_char_list = []
-    for char in pascal_or_camel_cased_string:
+    for char in pascal_or_camel_cased_string
     def char.isupper():
         pass
