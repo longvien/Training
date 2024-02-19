89509c07f2b4f67d029516a81fc63966bed51bd2
Long Vien <vienngoclongvn@gmail.com>
2024-02-19 21:21:26 +0100

    completed exercise

--- python2024/Learn_Python_List_Comprehension_by_Building_a_Case_Converter_Program.py
+++ python2024/Learn_Python_List_Comprehension_by_Building_a_Case_Converter_Program.py
@@ -7,5 +7,5 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
         else:
             snake_cased_char_list.append(char)
     snake_cased_string = ''.join(snake_cased_char_list)
-    clean_snake_cased_string = snake_cased_string.strip('_')
+    clean_snake_cased_string = snake_cased_string.strip('')
     return clean_snake_cased_string
