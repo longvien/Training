c62502bfddb3a98af07dd0124e7bef40e0e8a0dc
Long Vien <vienngoclongvn@gmail.com>
2024-02-26 21:45:45 +0100

    completed exercise

--- python2024/learn_regular_expressions_by_building_a_password_generator.py
+++ python2024/learn_regular_expressions_by_building_a_password_generator.py
@@ -1,7 +1,8 @@
 import secrets
 import string
 
-def generate_password(length):
+
+def generate_password(length, nums, special_chars, uppercase, lowercase):
     # Define the possible characters for the password
     letters = string.ascii_letters
     digits = string.digits
@@ -11,4 +12,9 @@ def generate_password(length):
     all_characters = letters + digits + symbols
     password = ''
     # Generate password
-    
+    for _ in range(length):
+        password += secrets.choice(all_characters)
+    return password
+
+# new_password = generate_password(8)
+# print(new_password)
