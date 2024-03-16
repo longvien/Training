fa0b5d0e711a1f300d43e807a22429b506c812fb
Long Vien <vienngoclongvn@gmail.com>
2024-03-16 22:04:09 +0100

    completed exercise

--- python2024/learn_regular_expressions_by_building_a_password_generator.py
+++ python2024/learn_regular_expressions_by_building_a_password_generator.py
@@ -4,6 +4,7 @@ import string
 
 
 def generate_password(length, nums, special_chars, uppercase, lowercase):
+   
     # Define the possible characters for the password
     letters = string.ascii_letters
     digits = string.digits
@@ -17,25 +18,22 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
         # Generate password
         for _ in range(length):
             password += secrets.choice(all_characters)
-       
+        
         constraints = [
             (nums, r'\d'),
-            (lowercase, r'[a-z]'),
-            (uppercase, r'[A-Z]'),            
-            (special_chars, fr'[{symbols}]')            
+            (special_chars, fr'[{symbols}]'),
+            (uppercase, r'[A-Z]'),
+            (lowercase, r'[a-z]')
         ]
 
-        # Check constraints
-        count = 0
+        # Check constraints        
         if all(
-            [
-                constraint <= len(re.findall(pattern, password))
-                for constraint, pattern in constraints
-            ]
+            constraint <= len(re.findall(pattern, password))
+            for constraint, pattern in constraints
         ):
             break
     
     return password
-
-# new_password = generate_password(8)
-# print(new_password)
+    
+new_password = generate_password(8, 1, 1, 1, 1)
+print(new_password)
