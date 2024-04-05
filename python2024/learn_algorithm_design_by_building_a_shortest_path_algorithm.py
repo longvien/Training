5d1aac557134de0f327c17aae364b0c3b4eddeb3
Long Vien <vienngoclongvn@gmail.com>
2024-04-05 22:53:28 +0200

    completed exercise

--- python2024/learn_algorithm_design_by_building_a_shortest_path_algorithm.py
+++ python2024/learn_algorithm_design_by_building_a_shortest_path_algorithm.py
@@ -2,8 +2,15 @@ my_graph = {
     'A': [('B', 3), ('D', 1)],
     'B': [('A', 3), ('C', 4)],
     'C': [('B', 4), ('D', 7)],
-    'D': [('A', 1), ('C', 5)]
+    'D': [('A', 1), ('C', 7)]
 }
 
 def shortest_path(graph, start):
     unvisited = []
+    distances = {}
+    for node in graph:
+        unvisited.append(node)
+        if node == start:
+            distances[node] = 0
+        else:
+            distances[node] = float('inf')    
