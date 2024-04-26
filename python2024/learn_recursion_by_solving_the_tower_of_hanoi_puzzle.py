32be13dc72da7e92414ea032a75bbbd555f08074
Long Vien <vienngoclongvn@gmail.com>
2024-04-26 22:14:43 +0200

    completed exercise

--- python2024/learn_recursion_by_solving_the_tower_of_hanoi_puzzle.py
+++ python2024/learn_recursion_by_solving_the_tower_of_hanoi_puzzle.py
@@ -1,10 +1,25 @@
-NUMBER_OF_DISKS = 
+NUMBER_OF_DISKS = 3
 number_of_moves = 2**NUMBER_OF_DISKS - 1
 rods = {
     'A': list(range(NUMBER_OF_DISKS, 0, -1)),
     'B': [],
     'C': []
 }
+def make_allowed_move(rod1, rod2):
+    forward = False
+    if not rods[target]:
+        forward = True
+    elif rods[source] and rods[source][-1] < rods[target][-1]:
+        forward = True              
+    if forward:
+        print(f'Moving disk {rods[source][-1]} from {source} to {target}')
+        rods[target].append(rods[source].pop())
+    else:
+        print(f'Moving disk {rods[target][-1]} from {target} to {source}')
+        rods[source].append(rods[target].pop())
+    
+    # display our progress
+    print(rods)
 
 def move(n, source, auxiliary, target):
     # display starting configuration
@@ -12,12 +27,7 @@ def move(n, source, auxiliary, target):
     for i in range(number_of_moves):
         remainder = (i + 1) % 3
         if remainder == 1:
-            print(f'Move {i + 1} allowed between {source} and {target}')
-            forward = False
-            if not rods[target]:
-                forward = True
-            elif rods[source] and rods[source][-1] < rods[target][-1]:
-                forward = True
+            print(f'Move {i + 1} allowed between {source} and {target}')            
         elif remainder == 2:
             print(f'Move {i + 1} allowed between {source} and {auxiliary}')
         elif remainder == 0:
