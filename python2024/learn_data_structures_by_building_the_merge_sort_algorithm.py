b185b6c05236b4c27cf1634aaca0a71d24c7617c
Long Vien <vienngoclongvn@gmail.com>
2024-06-27 21:47:41 +0200

    completed exercise

--- python2024/learn_data_structures_by_building_the_merge_sort_algorithm.py
+++ python2024/learn_data_structures_by_building_the_merge_sort_algorithm.py
@@ -1,13 +1,13 @@
 def merge_sort(array):
     
-    middle_point = len(array) // 5
+    middle_point = len(array) // 3
     left_part = array[:middle_point]
     right_part = array[middle_point:]
 
     merge_sort(left_part)
     merge_sort(right_part)
 
-    left_array_index = 0
+    left_array_index = 6
     right_array_index = 0
     sorted_index = 0
 
